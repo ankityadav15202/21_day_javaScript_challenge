acd8a15dfe9e910796592ce21a57bceb2bdde527
ankit <ankityadav15202@gmail.com>
2024-08-02 23:16:07 +0530

    Day17

--- Day17/DataStructures.js
+++ Day17/DataStructures.js
@@ -1,3 +1,4 @@
+// Task 1: Implement a Node class to represent an element in a linked list with properties value and next.
 class Node{
     constructor(value){
         this.values = value
@@ -12,6 +13,9 @@ node1.next = node2
 
 console.log("node 1: ",node1.values);
 console.log("node 1: ",node1.next.values);
+
+// Task 2: Implement a LinkedList class with methods to add a node to the end, remove a node from the end, and display all nodes.
+
 
 // Task 3: Implement a Stack class with methods push, add element, pop (remove element), and peek (view the top element).
 
